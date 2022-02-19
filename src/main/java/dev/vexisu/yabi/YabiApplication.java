828c4b65478e977f102c823009601ef09f4d9ae9
Maciej Waleryn <xvexisu@gmail.com>
2022-02-19 18:14:15 +0100

    Initialize gradle project

---
+++ src/main/java/dev/vexisu/yabi/YabiApplication.java
@@ -0,0 +1,13 @@
+package dev.vexisu.yabi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YabiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YabiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/vexisu/yabi/YabiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vexisu.yabi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YabiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
