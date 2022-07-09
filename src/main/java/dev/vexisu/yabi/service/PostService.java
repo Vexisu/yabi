3d5a31e01f464ff34d144589cf8205f31763030c
Maciej Waleryn <xvexisu@gmail.com>
2022-07-09 20:29:26 +0200

    Remove unnecessary @Autowired

--- src/main/java/dev/vexisu/yabi/service/PostService.java
+++ src/main/java/dev/vexisu/yabi/service/PostService.java
@@ -1,16 +1,16 @@
 package dev.vexisu.yabi.service;
 
 import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
 import dev.vexisu.yabi.impl.Post;
 import dev.vexisu.yabi.repository.PostRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 @Service
 public class PostService {
 	private final PostRepository postRepository;
 
-	@Autowired
 	public PostService(PostRepository postRepository) {
 		this.postRepository = postRepository;
 	}
