0310d446814667f2800f0d62f88a876f2b97a1ca
Maciej Waleryn <xvexisu@gmail.com>
2022-08-16 21:11:13 +0200

    Fix bug with displaying a title instead of a description on index site.

--- src/main/java/dev/vexisu/yabi/controller/post/ViewPostController.java
+++ src/main/java/dev/vexisu/yabi/controller/post/ViewPostController.java
@@ -1,19 +1,19 @@
 package dev.vexisu.yabi.controller.post;
 
 import java.util.Optional;
-import dev.vexisu.yabi.impl.Post;
-import dev.vexisu.yabi.service.PostService;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import dev.vexisu.yabi.impl.Post;
+import dev.vexisu.yabi.service.PostService;
+
 @Controller
 public class ViewPostController {
 	private final PostService postService;
 
-	@Autowired
 	public ViewPostController(PostService postService) {
 		this.postService = postService;
 	}
