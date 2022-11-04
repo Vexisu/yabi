9f4d6e75beaa802b91d0bcfa7488678e138234b6
Maciej Waleryn <xvexisu@gmail.com>
2022-11-04 13:37:46 +0100

    Add Spring Security configuration. Fix post view template.

---
+++ src/main/java/dev/vexisu/yabi/config/WebSecurityConfig.java
@@ -0,0 +1,22 @@
+package dev.vexisu.yabi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class WebSecurityConfig {
+
+    @Bean
+    public PasswordEncoder getBCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.build();
+    }
+
+}
