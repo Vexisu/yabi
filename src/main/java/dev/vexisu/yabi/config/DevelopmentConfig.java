0463bd229379f6fd2b9238eb34ff08d6031c661c
vexisu <xvexisu@gmail.com>
2024-04-06 22:53:26 +0200

    fix(backend): use PasswordEncoder to encode test user's password

--- src/main/java/dev/vexisu/yabi/config/DevelopmentConfig.java
+++ src/main/java/dev/vexisu/yabi/config/DevelopmentConfig.java
@@ -4,6 +4,7 @@ import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import dev.vexisu.yabi.impl.Post;
 import dev.vexisu.yabi.impl.User;
@@ -15,17 +16,20 @@ import dev.vexisu.yabi.repository.UserRepository;
 public class DevelopmentConfig {
 	private final PostRepository postRepository;
 	private final UserRepository userRepository;
+	private final PasswordEncoder passwordEncoder;
 
-	public DevelopmentConfig(PostRepository postRepository, UserRepository userRepository) {
+	public DevelopmentConfig(PostRepository postRepository, UserRepository userRepository,
+			PasswordEncoder passwordEncoder) {
 		this.postRepository = postRepository;
 		this.userRepository = userRepository;
+		this.passwordEncoder = passwordEncoder;
 	}
 
 	@EventListener(ApplicationReadyEvent.class)
 	public void addTestData() {
 		var user = new User();
 		user.setUsername("user");
-		user.setHashedPassword("qwertyuiop");
+		user.setHashedPassword(passwordEncoder.encode("qwertyuiop"));
 		userRepository.save(user);
 		var post = new Post();
 		post.setId(1);
