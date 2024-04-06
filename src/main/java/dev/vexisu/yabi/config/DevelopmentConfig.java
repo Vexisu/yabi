4843576ee342f6155ea3bd6ed9dff4b019d25629
vexisu <xvexisu@gmail.com>
2024-04-06 22:40:29 +0200

    refactor(backend): remove unused import

--- src/main/java/dev/vexisu/yabi/config/DevelopmentConfig.java
+++ src/main/java/dev/vexisu/yabi/config/DevelopmentConfig.java
@@ -1,7 +1,6 @@
 package dev.vexisu.yabi.config;
 
 import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
